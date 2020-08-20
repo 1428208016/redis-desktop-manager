5cce1f9ca5348142a1d7a8c2b0e44c5ced31eac0
凌振 <1428208016@qq.com>
2020-08-20 14:05:22 +0800

    fastjson

--- common/src/main/java/com/lingzhen/myproject/common/Test.java
+++ common/src/main/java/com/lingzhen/myproject/common/Test.java
@@ -1,19 +1,7 @@
 package com.lingzhen.myproject.common;
 
-import com.lingzhen.myproject.common.util.NetworkRequestOperator;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
     public static void main(String[] args) {
-        NetworkRequestOperator networkRequestOperator = new NetworkRequestOperator();
-        Map map = new HashMap();
-        map.put("content-type","application/json");
-        networkRequestOperator.setHeaders(map);
 
-        String url = "http://apidev.bbapp.cn/bb/serverapi/activity/valentinesDay/express?text=12&userId=21";
-        String str = networkRequestOperator.post(url,"");
-        System.out.println(str);
     }
 }
