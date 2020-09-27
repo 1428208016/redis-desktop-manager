6ea356ec8aeee22ede36149da6fd292a3dd68001
凌振 <1428208016@qq.com>
2020-09-27 21:35:27 +0800

    show

--- lifefolder/src/main/java/com/lingzhen/myproject/lifefolder/controller/FileUploadController.java
+++ lifefolder/src/main/java/com/lingzhen/myproject/lifefolder/controller/FileUploadController.java
@@ -2,6 +2,7 @@ package com.lingzhen.myproject.lifefolder.controller;
 
 import com.lingzhen.myproject.lifefolder.pojo.Result;
 import com.lingzhen.myproject.lifefolder.service.FileUploadService;
+import com.lingzhen.myproject.lifefolder.util.HttpServletUtil;
 import com.lingzhen.myproject.lifefolder.util.VerifyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("file")
 public class FileUploadController {
 
     @Autowired
     private FileUploadService fileUploadService;
+
+
+    @RequestMapping(value = "/show", method = RequestMethod.POST)
+    public Result show() {
+        Result result = new Result();
+        try {
+            Map map = HttpServletUtil.getRequestParameter();
+
+            File file = new File(map.get("path").toString());
+            File[] fileList = file.listFiles();
+            List<String> retData = new ArrayList<>();
+            for (File temp : fileList) {
+                retData.add(temp.getName());
+            }
+            result.setData(retData);
+        }catch (Exception e){
+            result.setError(e.getMessage());
+        }
+        return result;
+    }
+
 
     //上传
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
